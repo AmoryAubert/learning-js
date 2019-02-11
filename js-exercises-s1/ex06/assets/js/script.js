2749c8bbc669d2a776b5790c7bc4d2c57cd83e1c
Amory Aubert <amory.aubert@gmail.com>
2019-02-11 14:06:13 +0100

    serie 1

---
+++ js-exercises-s1/ex06/assets/js/script.js
@@ -0,0 +1,5 @@
+function valider(){
+    nb1 = document.getElementById("premier_nombre").value;
+    nb2 = document.getElementById("deuxieme_nombre").value;
+    window.alert("reste de division : "+nb1%nb2);
+}
