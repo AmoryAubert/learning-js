c7b01f7b72dfddf975a6e4bd6d77a12acef62663
Amory Aubert <amory.aubert@gmail.com>
2019-02-19 16:59:09 +0100

    fin journee

--- pendu/assets/js/pendu.js
+++ pendu/assets/js/pendu.js
@@ -1,7 +1,8 @@
 let mot = ["B","O","N","J","O","U","R"];
 let lettreTrouve = ["_","_","_","_","_","_","_"];
 let lettreEntree=[];
-
+let lettreErreur=[];
+let erreur=0;
 let essaie=0;
 const alpha = "abcdefghijklmnopqrstuvwxyz";
 
@@ -18,20 +19,33 @@ function guestLetter(e){
             mot[i] = mot[i].toLowerCase();
             if (lettre === mot[i]){
                 lettreTrouve[i] = mot[i].toUpperCase();
-            } else {
-                for (let j=0; j < mot.length; j++){
-                    if (lettreEntree[j]===mot[i]){
-                        console.log("patate");
-                    } else {
-                        lettreEntree[essaie]=lettre;
-                    }    
+            }
+            if ((lettreEntree.toString().includes(lettre))==false){
+                if ((lettreEntree.toString().includes(mot[i]))){
+                    lettreErreur[essaie]=lettre;
+                    lettreErreur=lettreErreur.toString();
+                    lettreErreur=lettreErreur.replace(/,/g,"");
+                    lettreErreur=lettreErreur.split("");
+                    lettreErreur.sort();
                 }
-            }       
+                lettreEntree[essaie]=lettre;
+                lettreEntree=lettreEntree.toString();
+                lettreEntree=lettreEntree.replace(/,/g,"");
+                lettreEntree=lettreEntree.split("");
+                lettreEntree.sort();    
+            }
+            mot[i] = mot[i].toUpperCase();
         }
+        
+        //lettreEntree.toString();
+        console.log("entree: "+lettreEntree);
+        console.log("erreur: "+lettreErreur);
         essaie++;
         for (let i=0; i < lettreTrouve.length; i++){
             document.getElementById("jeux").innerHTML += lettreTrouve[i]+" "; 
         }
-        console.log(lettreEntree);
+        if (lettreTrouve.toString()===mot.toString()){
+            document.getElementById("win").innerHTML="Vous avez gagné !";
+        }
     }
 }
