dc0a6d42904e93bc0a2440df5698d4335889202c
Amory Aubert <amory.aubert@gmail.com>
2019-02-22 16:58:50 +0100

    fin journee

--- js-exercises-s3/assets/js/ex06.js
+++ js-exercises-s3/assets/js/ex06.js
@@ -1,8 +1,7 @@
 let y = document.querySelectorAll("img");
 let x=0;
 for (let i=0 ; i<y.length; i++){
-    y[i].addEventListener("mouseenter", changeImage);
-    y[i].addEventListener("mouseout", changeImage);
+    y[i].addEventListener("click", changeImage);
     function changeImage() {
         if (x===0){
             y[i].setAttribute("src", `assets/img/image${i+1}_2.jpg`);
