fcb84339f2ff5eea2933ebd864f4c3f96ec3f8ac
Amory Aubert <amory.aubert@gmail.com>
2019-03-11 17:00:53 +0100

    fin journee

---
+++ datavisualisation-challenge/assets/script.js
@@ -0,0 +1,20 @@
+//https://inside.becode.org/api/v1/data/random.json
+/*
+var dataPoints = [];
+$.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=10&length=10&type=json", function(data) {  
+    $.each(data, function(key, value){
+        dataPoints.push({x: value[0], y: parseInt(value[1])});
+    });
+    chart = new CanvasJS.Chart("chartContainer",{
+        title:{
+            text:"Live Chart with dataPoints from External JSON"
+        },
+        data: [{
+        type: "line",
+        dataPoints : dataPoints,
+        }]
+    });
+    chart.render();
+    updateChart();
+});
+*/
