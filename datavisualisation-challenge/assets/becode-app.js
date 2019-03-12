7dc5cb8497792449db88c54eddc7de096f63407f
Amory Aubert <amory.aubert@gmail.com>
2019-03-12 22:22:29 +0100

    refresh graph1 + test graph3

--- datavisualisation-challenge/assets/becode-app.js
+++ datavisualisation-challenge/assets/becode-app.js
@@ -12,8 +12,10 @@ let newDiv = document.createElement("div");
 let newContent = document.createTextNode("");
 newDiv.appendChild(newContent);
 document.getElementById("bodyContent").insertBefore(newDiv, theFirstChild);
-newDiv.setAttribute("id", "bar-chart")
-let dataPoints=[];
+newDiv.setAttribute("id", "bar-chart");
+let svg = dimple.newSvg("div#bar-chart", 800, 400);
+function updateChart(){
+        let dataPoints=[];
         async function getData() {
             let response = await fetch('https://inside.becode.org/api/v1/data/random.json');
             let data = await response.json();
@@ -23,13 +25,18 @@ let dataPoints=[];
             for (let i in data){
                     dataPoints.push({"Année":data[i][0], "Taux de criminalité":data[i][1]});
             }
-            let svg = dimple.newSvg("div#bar-chart", 800, 400);
-                    let chart = new dimple.chart(svg, dataPoints);
+            let chart = new dimple.chart(svg, dataPoints);
                     chart.addCategoryAxis("x", "Année");
                     chart.addMeasureAxis("y", "Taux de criminalité");
                     chart.addSeries(null, dimple.plot.bar);
                     chart.draw();
+                    setTimeout(function(){
+                        chart.svg.selectAll("*").remove();
+                        updateChart();
+                    }, 2000);
         })
+}
+updateChart();
 // DEUXIEME GRAPHIQUE
 // TROISIEME GRAPHIQUE
 let table2 = document.getElementById("table2");
