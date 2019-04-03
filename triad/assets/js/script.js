cf9629b4ca1f5ed96037ef217b6a51116a5b84e7
Amory Aubert <amory.aubert@gmail.com>
2019-04-03 14:27:20 +0200

    triad

--- triad/assets/js/script.js
+++ triad/assets/js/script.js
@@ -10,13 +10,19 @@ function drop(ev) {
   ev.preventDefault();
   let data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
+  document.getElementById(data).removeAttribute("draggable");
 }
-//let dropZone=document.querySelectorAll('.zoneCarte');
-//for (let i=0; i<dropZone.length;i++){
-//    dropZone[i].addEventListener('dragover', allowDrop(event));
-//    dropZone[i].addEventListener('drop', drop(event));
-//}
-//let cartes=document.querySelectorAll('.img-container');
-//for (let i=0; i<cartes.length;i++){
-//    cartes[i].addEventListener('dragstart', drag(event));
-//}
+let dropZone=document.querySelectorAll('.zoneCarte');
+for (let i=0; i<dropZone.length;i++){
+    dropZone[i].addEventListener('dragover', function(event){allowDrop(event)});
+    dropZone[i].addEventListener('drop', function(event){drop(event)});
+}
+let cartes=document.querySelectorAll('.img-container');
+for (let i=0; i<cartes.length;i++){
+    cartes[i].addEventListener('dragstart', function(event){drag(event)});
+}
+for (let i=0; i<cartes.length;i++){
+    cartes[i].addEventListener('drop', function(e) {
+        e.stopPropagation();
+    });
+}
