c0adc1da780bad28c4f6c66887c2abee4dc26b27
Amory Aubert <amory.aubert@gmail.com>
2019-04-03 12:19:47 +0200

    triad

--- triad/assets/js/script.js
+++ triad/assets/js/script.js
@@ -1,82 +1,22 @@
-class Character {
-    constructor(name, level, life, nameWeapon, damageWeapon) {
-        this.name = name;
-        this.level = level;
-        this.life = life;
-        this.damage = level * damageWeapon;
-        weapon : {
-                this.nameWeapon = nameWeapon;
-                this.damageWeapon = damageWeapon;
-        }
-        this.attack = function(name){
-            console.log(this.name + " attaque " + name + " avec " + this.nameWeapon);
-        }
-        this.status = function(name){
-            this.life = this.life - name.damage;
-            //this.life = this.life - this.receiveDamage();
-            console.log(this.name +" a maintenant "+this.life+" point(s) de vie.");
-        }
-        this.receiveDamage = function(name){
-            console.log(this.name + " inflige " + this.damage + " de dégats");
-            return this.damage;
-        }
-    }
+function allowDrop(ev) {
+  ev.preventDefault();
 }
-let mainCharacter = new Character("Arthour",20,500,"Kwuillère",10);
-let opponentCharacter = new Character("Burgonde",15,375,"Assiette Pour Fromage",15);
-mainCharacter.attack(opponentCharacter.name);
-mainCharacter.receiveDamage(opponentCharacter);
-opponentCharacter.status(mainCharacter);
-//let shop = [
-//    {
-//        title : "Ragnarok",
-//        type: "Marteau",
-//        physic: 10,
-//        magic: 5,
-//        minLevel: 10,
-//        available: 0
-//    },
-//    {
-//        title : "Excalibur",
-//        type: "Épée",
-//        physic: 15,
-//        magic: 0,
-//        minLevel: 10,
-//        available: 1
-//    },
-//    {
-//        title : "Épée en bois",
-//        type: "Épée",
-//        physic: 2,
-//        magic: 0,
-//        minLevel: 1,
-//        available: 1
-//    },
-//    {
-//        title : "Baguette d'apprenti",
-//        type: "Baguette",
-//        physic: 0,
-//        magic: 2,
-//        minLevel: 1,
-//        available: 1
-//    },
-//];
-//function allItem(){
-//    for(let i in shop){
-//          console.log(shop[i]);  
-//    }
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+//let dropZone=document.querySelectorAll('.zoneCarte');
+//for (let i=0; i<dropZone.length;i++){
+//    dropZone[i].addEventListener('dragover', allowDrop(event));
+//    dropZone[i].addEventListener('drop', drop(event));
 //}
-//function availableItem(){
-//    for(let i in shop){
-//        if (shop[i].available == 1){
-//          console.log(shop[i]);  
-//        }
-//    }
-//}
-//function availableItemAtLv10(){
-//    for(let i in shop){
-//        if ((shop[i].minLevel >= 10)&&(shop[i].available == 1)){
-//          console.log(shop[i]);  
-//        }
-//    }
+//let cartes=document.querySelectorAll('.img-container');
+//for (let i=0; i<cartes.length;i++){
+//    cartes[i].addEventListener('dragstart', drag(event));
 //}
